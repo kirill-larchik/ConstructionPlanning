87eff588bb77124586ae1622803e4cd4c3db117d
Kiryl Larchyk <kiryl.larchyk@gmail.com>
2022-03-31 01:08:36 +0300

    Refactor context.

--- src/ConstructionPlanning.DataLayer/DbContext/ConstructionPlanningDbContext.cs
+++ src/ConstructionPlanning.DataLayer/DbContext/ConstructionPlanningDbContext.cs
@@ -8,20 +8,20 @@ namespace ConstructionPlanning.DataLayer.DbContext
     /// </summary>
     public class ConstructionPlanningDbContext : Microsoft.EntityFrameworkCore.DbContext
     {
-        public DbSet<ConstructionObject> ConstructionObjects { get; set; }
+        public DbSet<ConstructionObject>? ConstructionObjects { get; set; }
 
-        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Customer>? Customers { get; set; }
 
-        public DbSet<Delivery> Deliveries { get; set; }
+        public DbSet<Delivery>? Deliveries { get; set; }
 
-        public DbSet<Project> Projects { get; set; }
+        public DbSet<Project>? Projects { get; set; }
 
-        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Resource>? Resources { get; set; }
 
-        public DbSet<ResourcePerObject> ResourcesPerObject { get; set; }
+        public DbSet<ResourcePerObject>? ResourcesPerObject { get; set; }
 
-        public DbSet<ResourceType> ResourceTypes { get; set; }
+        public DbSet<ResourceType>? ResourceTypes { get; set; }
 
-        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Sale>? Sales { get; set; }
     }
 }
